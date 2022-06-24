1dab36c22dd47f91808bf415dc243cc063f1841c
francoisdomecq <fradomecq@gmail.com>
2022-06-24 15:28:07 +0200

    Map class to function

--- Frontend/screens/MapScreen/Map.tsx
+++ Frontend/screens/MapScreen/Map.tsx
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { View, StyleSheet, ActivityIndicator } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import MapView, { Polyline, Marker } from 'react-native-maps';
 import { NavigationProps } from '../../navigation/app-stacks';
 import { Ionicons } from '@expo/vector-icons';
+import { getDistance } from 'geolib';
 
 import CustomMarker from '../../components/CustomMarker';
 import ModalMap from '../../components/ModalMap';
+import Quartier from '../../components/Quartier';
 
 import MarkerApi from '../../services/point_interet.api_service';
 import MarkerModel from '../../services/point_interet.model';
 import City from '../../services/city.model';
 import QuartierModel from '../../services/quartier.model';
-import Quartier from '../../components/Quartier';
 
 interface MapProps extends NavigationProps {
   latitude: number;
@@ -23,187 +24,180 @@ interface MapProps extends NavigationProps {
   setLocation: Function;
 }
 
-interface MapState {
-  markers: Array<MarkerModel>;
-  constantMarkers: Array<MarkerModel>;
-  themeChoisi: Array<string>;
-  displayModal: boolean;
-}
-export default class Map extends Component<MapProps, MapState> {
-  constructor(public props: MapProps) {
-    super(props);
-    this.state = {
-      markers: [],
-      constantMarkers: [],
-      themeChoisi: [],
-      displayModal: false
-    };
-  }
+export default function MapFunction(props: MapProps) {
+  const [markers, SetMarkers] = useState<Array<MarkerModel>>([]);
+  const [constantMarkers, setConstantMarkers] = useState<Array<MarkerModel>>(
+    []
+  );
+  const [themeChoisi, setThemeChoisi] = useState<Array<string>>([]);
+  const [displayModal, setDisplayModal] = useState<boolean>(false);
 
-  setModalVisible = (modalVisible: boolean) => {
-    this.setState({ displayModal: modalVisible });
+  const setModalVisible = (modalVisible: boolean) => {
+    setDisplayModal(modalVisible);
   };
 
-  selectTheme = (theme: string) => {
-    if (this.state.themeChoisi.includes(theme)) {
-      const newThemesArray = [...this.state.themeChoisi];
+  const selectTheme = (theme: string) => {
+    if (themeChoisi.includes(theme)) {
+      const newThemesArray = [...themeChoisi];
       newThemesArray.splice(newThemesArray.indexOf(theme));
       if (newThemesArray.length === 0) {
-        this.setState({ markers: this.state.constantMarkers });
-        this.setState({ themeChoisi: newThemesArray });
+        SetMarkers(constantMarkers);
+        setThemeChoisi(newThemesArray);
       } else {
-        this.setState({ themeChoisi: newThemesArray });
-        this.filter();
+        setThemeChoisi(newThemesArray);
+        filter();
       }
     } else {
       if (theme !== 'Annuler') {
-        const newTheme = [...this.state.themeChoisi, theme];
-        this.setState({ themeChoisi: newTheme });
-        this.filter();
+        const newTheme = [...themeChoisi, theme];
+        setThemeChoisi(newTheme);
+        filter();
       } else {
-        this.setState({ themeChoisi: [] });
-        this.setState({ markers: this.state.constantMarkers });
-        this.setState({ displayModal: false });
+        setThemeChoisi([]);
+        SetMarkers(constantMarkers);
+        setDisplayModal(false);
       }
     }
   };
 
-  filter() {
-    const filteredMarkers = this.state.constantMarkers.filter((marker) => {
-      if (this.state.themeChoisi.includes(marker.theme)) return marker;
+  function filter() {
+    const filteredMarkers = constantMarkers.filter((marker) => {
+      if (themeChoisi.includes(marker.theme)) return marker;
     });
-    this.setState({ markers: filteredMarkers });
+    SetMarkers(filteredMarkers);
   }
 
-  loadMarkers() {
-    this.props.quartiers.forEach((quartier) => {
-      quartier.listePI.map((pi: string) => {
-        MarkerApi.getPIFromId(pi).then((marker) => {
-          this.setState({ markers: [...this.state.markers, marker] });
-          this.setState({
-            constantMarkers: [...this.state.constantMarkers, marker]
-          });
-        });
-      });
+  function loadMarkers() {
+    // props.quartiers.forEach((quartier) => {
+    //   quartier.listePI.map((pi: string) => {
+    //     MarkerApi.getPIFromId(pi).then((marker) => {
+    //       SetMarkers([...markers, marker]);
+    //       setConstantMarkers([...constantMarkers, marker]);
+    //     });
+    //   });
+    // });
+    MarkerApi.getPI().then((markers) => {
+      SetMarkers(markers);
+      setConstantMarkers(markers);
     });
   }
 
-  componentDidMount() {
-    this.loadMarkers();
-  }
+  useEffect(() => {
+    loadMarkers();
+  }, []);
+  return markers ? (
+    <View
+      style={styles.container}
+      // onTouchStart={() =>
+      //   this.props.navigation.setOptions({
+      //     headerShown: true
+      //   })
+      // }
+      // onTouchEnd={() =>
+      //   this.props.navigation.setOptions({
+      //     headerShown: false
+      //   })
+      // }
+    >
+      <MapView
+        initialRegion={{
+          latitude: props.latitude,
+          longitude: props.longitude,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1
+        }}
+        mapPadding={{ top: 5, right: 5, left: 5, bottom: 5 }}
+        onUserLocationChange={(user) => {
+          //On déplace la caméra du joueur vers ses coordoonées (latitude, longitude)
+          const latitude = user.nativeEvent.coordinate.latitude;
+          const longitude = user.nativeEvent.coordinate.longitude;
+          props.setLocation(latitude, longitude);
+        }}
+        camera={{
+          center: {
+            longitude: props.longitude,
+            latitude: props.latitude
+          },
+          pitch: 70,
+          heading: 0,
+          zoom: 15,
+          altitude: 0
+        }}
+        // Only on iOS MapKit, in meters. The property is ignored by Google Maps.
 
-  // componentDidUpdate() {
-  //   this.loadMarkers();
-  // }
-
-  render() {
-    const { markers, displayModal, themeChoisi } = this.state;
-    const {
-      latitude,
-      longitude,
-      userType,
-      quartiers,
-      cityPicked,
-      setLocation
-    } = this.props;
-    console.log(markers);
-    return markers ? (
-      <View
-        style={styles.container}
-        // onTouchStart={() =>
-        //   this.props.navigation.setOptions({
-        //     headerShown: true
-        //   })
-        // }
-        // onTouchEnd={() =>
-        //   this.props.navigation.setOptions({
-        //     headerShown: false
-        //   })
-        // }
+        style={styles.map}
+        // showsUserLocation={true}
+        showsCompass={true}
+        showsMyLocationButton={true}
+        // zoomEnabled={false}
+        // pitchEnabled={false}
       >
-        <MapView
-          initialRegion={{
-            latitude: latitude,
-            longitude: longitude,
-            latitudeDelta: 0.1,
-            longitudeDelta: 0.1
+        <Marker
+          coordinate={{
+            latitude: props.latitude,
+            longitude: props.longitude
           }}
-          mapPadding={{ top: 5, right: 5, left: 5, bottom: 5 }}
-          onUserLocationChange={(user) => {
-            //On déplace la caméra du joueur vers ses coordoonées (latitude, longitude)
-            const latitude = user.nativeEvent.coordinate.latitude;
-            const longitude = user.nativeEvent.coordinate.longitude;
-            setLocation(latitude, longitude);
-          }}
-          camera={{
-            center: {
-              longitude: longitude,
-              latitude: latitude
-            },
-            pitch: 70,
-            heading: 0,
-            zoom: 15,
-            altitude: 0
-          }}
-          // Only on iOS MapKit, in meters. The property is ignored by Google Maps.
-
-          style={styles.map}
-          // showsUserLocation={true}
-          showsCompass={true}
-          showsMyLocationButton={true}
-          // zoomEnabled={false}
-          // pitchEnabled={false}
-        >
-          <Marker
-            coordinate={{
-              latitude: latitude,
-              longitude: longitude
-            }}
-          />
-          {quartiers.map((quartier) => {
-            return (
-              <Quartier
-                key={quartier._id}
-                position={{ latitude, longitude }}
-                quartier={quartier}
-              />
-            );
-          })}
-
-          <Polyline coordinates={cityPicked.polygon} strokeWidth={2} />
-
-          {markers.map((marker) => {
-            return (
-              <CustomMarker
-                key={marker._id}
-                marker={marker}
-                navigation={this.props.navigation}
-              />
-            );
-          })}
-        </MapView>
-
-        <View style={styles.viewThemeChoisi}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => this.setModalVisible(true)}
-          >
-            <Ionicons name="ios-settings-sharp" size={30} color="#808080" />
-          </TouchableOpacity>
-        </View>
-        <ModalMap
-          setModalVisible={this.setModalVisible}
-          modalVisible={displayModal}
-          selectTheme={this.selectTheme}
-          themeChoisi={this.state.themeChoisi}
         />
+        {props.quartiers.map((quartier) => {
+          return (
+            <Quartier
+              key={quartier._id}
+              position={{
+                latitude: props.latitude,
+                longitude: props.longitude
+              }}
+              quartier={quartier}
+            />
+          );
+        })}
+
+        <Polyline coordinates={props.cityPicked.polygon} strokeWidth={2} />
+
+        {/* {markers.map((marker) => {
+          return getDistance(
+            { latitude: latitude, longitude: longitude },
+            { latitude: marker.latitude, longitude: marker.longitude }
+          ) < 1000 ? (
+            <CustomMarker
+              key={marker._id}
+              marker={marker}
+              navigation={this.props.navigation}
+            />
+          ) : null;
+        })} */}
+
+        {markers.map((marker) => {
+          return (
+            <CustomMarker
+              key={marker._id}
+              marker={marker}
+              navigation={props.navigation}
+            />
+          );
+        })}
+      </MapView>
+
+      <View style={styles.viewThemeChoisi}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setModalVisible(true)}
+        >
+          <Ionicons name="ios-settings-sharp" size={30} color="#808080" />
+        </TouchableOpacity>
       </View>
-    ) : (
-      <View>
-        <ActivityIndicator />
-      </View>
-    );
-  }
+      <ModalMap
+        setModalVisible={setModalVisible}
+        modalVisible={displayModal}
+        selectTheme={selectTheme}
+        themeChoisi={themeChoisi}
+        navigation={props.navigation}
+      />
+    </View>
+  ) : (
+    <View>
+      <ActivityIndicator />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
